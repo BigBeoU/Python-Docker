b225e04eae5159a423a25e874c922d9b29a76511
chaupham90 <chaupham.dev@gmail.com>
2023-04-30 18:55:40 +0200

    changed port'

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
 
 @app.route('/widgets')
 def get_widgets():
@@ -18,19 +20,20 @@ def get_widgets():
     )
     cursor = mydb.cursor()
 
-
     cursor.execute("SELECT * FROM widgets")
 
-    row_headers=[x[0] for x in cursor.description] #this will extract row headers
+    # this will extract row headers
+    row_headers = [x[0] for x in cursor.description]
 
     results = cursor.fetchall()
-    json_data=[]
+    json_data = []
     for result in results:
-        json_data.append(dict(zip(row_headers,result)))
+        json_data.append(dict(zip(row_headers, result)))
 
     cursor.close()
 
     return json.dumps(json_data)
+
 
 @app.route('/initdb')
 def db_init():
@@ -54,10 +57,12 @@ def db_init():
     cursor = mydb.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor.execute(
+        "CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
     cursor.close()
 
     return 'init database'
 
+
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0')
+    app.run(host='0.0.0.1')
